f7db1cc8398aa8dabf17c4d8bdede7471af83132
José Alvarez Martinez <97672139+JoseAlvarezMartinez@users.noreply.github.com>
2026-04-08 09:13:56 -0300

    fetch

--- js/index.js
+++ js/index.js
@@ -20,3 +20,46 @@ themeToggle.addEventListener("click", () => {
         document.body.dataset.theme = "dark"
     }
 })
+
+// Bloque 4 - Carga de datos (asincronia)
+
+async function fetchCountries(){
+    try{
+        const response = await fetch("./js/data.json")
+        
+        if(!response.ok) throw new Error("No se pudo cargar el archivo")
+        allCountries = await response.json()
+        displayCountries(allCountries)
+    
+    }catch(error){
+        flex.innerHTML = "<p>Error al cargar los datos</p>"
+    }
+}
+
+function displayCountries(countries){
+    flex.innerHTML = "";
+
+    if(countries.length === 0){
+        flex.innerHTML = "<p> No se encontraron resultados</p>"
+    }
+
+    countries.forEach(country => {
+        const card = document.createElement("article");
+        console.log(country);
+        
+        card.innerHTML = `
+        <img src="${country.flags.png}" alt="${country.name}"/>
+        <section>
+            <h2>${country.name}</h2>
+            <h3>Population: ${country.population}</h3>
+            <h3>Region: ${country.region}</h3>
+            <h3>Capital: ${country.capital} </h3>
+
+        </section>
+        `
+
+        flex.appendChild(card)
+    });
+}
+
+fetchCountries()
